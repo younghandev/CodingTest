a0f9de11d5c730459e639d4689f17fe5c5749664
younghandev <108641409+younghandev@users.noreply.github.com>
2022-10-04 13:53:58 +0900

    Update 삼각 달팽이.cs

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.cs
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.cs
@@ -2,29 +2,24 @@ using System;
 
 public class Solution {
     public int[] solution(int n) {
-        int size = 0;
-        for (int i = 1; i <= n; i++) size += i;
-        
+        int size = n * (n + 1) / 2;
         int[] answer = new int[size];
         
         int index = 0;
         int tempIndex = 1;
         int gap = 0;
         int dir = 1;
-        int tempN = n;
         int count = 0;
+        int maxCount = n;
         
         for (int i = 1; i <= size; i++) {
             answer[index] = i;
             
-            count++;
-            
-            if (count == tempN) {
-                tempN--;
+            if (++count == maxCount) {
+                maxCount--;
                 count = 0;
                 
-                dir--;
-                if (dir == -2) dir = 1;
+                if (--dir == -2) dir = 1;
                 
                 if (dir == 1 || dir == -1) tempIndex *= -1;
             }
