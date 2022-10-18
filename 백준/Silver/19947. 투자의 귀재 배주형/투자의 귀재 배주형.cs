72a526bf1b47bdde36d677e340cbfc65011c3948
younghandev <108641409+younghandev@users.noreply.github.com>
2022-10-18 12:17:29 +0900

    [Silver V] Title: 투자의 귀재 배주형, Time: 68 ms, Memory: 5720 KB -BaekjoonHub

---
+++ 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+class Program
+{
+    static void Main() {
+        int[] hy = ReadLine().Split().Select(int.Parse).ToArray();
+        
+        int h = hy[0];
+        int y = hy[1];
+        int[] sums = new int[11];
+        
+        sums[0] = h;
+        
+        for (int i = 1; i <= y; i++) {
+	        sums[i] = (int)(sums[i-1] * 1.05);
+	        
+	        if (i >= 3)
+    		    sums[i] = Math.Max(sums[i], (int)(sums[i-3] * 1.2));
+    		
+    		if (i >= 5)
+    			sums[i] = Math.Max(sums[i], (int)(sums[i-5] * 1.35));
+        }
+        
+        WriteLine(sums[y]);
+    }
+}
