db352e211993c72a240bcb38e30889182f93968d
younghandev <108641409+younghandev@users.noreply.github.com>
2022-11-08 11:17:51 +0900

    [Silver IV] Title: 수 찾기, Time: 184 ms, Memory: 26704 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cs
+++ 백준/Silver/1920. 수 찾기/수 찾기.cs
@@ -11,17 +11,17 @@ namespace CodingTest
         public static void Main()
         {
             int n = int.Parse(ReadLine());
-            List<int> a = ReadLine().Split().Select(int.Parse).ToList();
+            int[] a = ReadLine().Split().Select(int.Parse).ToArray();
             int m = int.Parse(ReadLine());
-            List<int> checks = ReadLine().Split().Select(int.Parse).ToList();
+            int[] checks = ReadLine().Split().Select(int.Parse).ToArray();
 
             StringBuilder sb = new StringBuilder();
 
-            a.Sort();
+            Array.Sort(a);
 
-            foreach(int check in checks)
+            foreach (int check in checks)
             {
-                sb.Append(a.BinarySearch(check) < 0 ? "0" : "1");
+                sb.Append(Array.BinarySearch(a, check) < 0 ? "0" : "1");
                 sb.Append("\n");
             }
 
